e7d04808b1e95cd66b179ea94e071321ba13c1c1
Marco Kroll <marco.h.kroll@gmail.com>
2023-06-25 21:39:18 +0200

    Settings: More autohandling in `settingsChanged`

--- settings.js
+++ settings.js
@@ -4,55 +4,72 @@
     settings.options = {
         tags: {
             get: () => Object.values(document.querySelectorAll('input[class="tagCheckbox"]:checked')).map(c => c.id),
+            getDom: () => Object.values(document.querySelectorAll('input[class="tagCheckbox"]:checked')).map(c => c.id),
             getUrl: () => (urlParams.get('tags') || '').split(',').filter(s => '' !== s) || settings.options.tag.default,
             set: (tags) => {
                 const tagSet = new Set(tags);
                 [...document.getElementsByClassName("tagCheckbox")].map(c => c.checked = tagSet.has(c.id));
                 urlParams.set('tags', [...tagSet].join(','));
             },
+            domEqualUrl: () => {
+                const urlTags = settings.options.tags.getUrl();
+                const domTags = settings.options.tags.getDom();
+                const intersection = intersect(domTags, urlTags);
+                return = (intersection.length === domTags.length &&
+                    domTags.length === urlTags.length);
+            },
             default: [],
         },
         randomize: {
             get: () => document.getElementById('randomizeExercises').checked? '1' : '0',
+            getDom: () => document.getElementById('randomizeExercises').checked? '1' : '0',
             getUrl: () => urlParams.get('randomize') || settings.options.randomize.default,
             getBool: () => document.getElementById('randomizeExercises').checked,
             set: (randomize) => {
                 document.getElementById('randomizeExercises').checked = (settings.options.randomize.default !== randomize);
                 urlParams.set('randomize', randomize);
             },
+            domEqualUrl: () => settings.options.randomize.getDom() === settings.options.randomize.getUrl(),
             default: '0',
         },
         hand: {
             get: () => document.querySelector('input[name="handedness"]:checked').value,
+            getDom: () => document.querySelector('input[name="handedness"]:checked').value,
             getUrl: () => urlParams.get('hand') || settings.options.hand.default,
             set: (hand) => {
                 if('left' === hand) leftHandedStenoKeyboard();
                 else rightHandedStenoKeyboard();
                 urlParams.set('hand', hand);
             },
+            domEqualUrl: () => settings.options.hand.getDom() === settings.options.hand.getUrl(),
             default: 'right',
         },
         scheduler: {
             get:  () => document.querySelector('input[name="trainingType"]:checked').value,
+            getDom:  () => document.querySelector('input[name="trainingType"]:checked').value,
             getUrl: () => urlParams.get('scheduler') || settings.options.scheduler.default,
             set: (scheduler) => {
                 radioSetter('trainingType', scheduler);
                 urlParams.set('scheduler', scheduler);
             },
+            domEqualUrl: () => settings.options.scheduler.getDom() === settings.options.scheduler.getUrl(),
             default: 'roundRobin',
         },
         failcount: {
             get: () => document.getElementById('failcount').value,
+            getDom: () => document.getElementById('failcount').value,
             getUrl: () => urlParams.get('failcount') || settings.options.failcount.default,
             set: (failcount) => {
                 document.getElementById('failcount').value = failcount;
                 urlParams.set('failcount', failcount);
                 changeMax(undefined, failcount);
             },
+            domEqualUrl: () => settings.options.failcount.getDom() === settings.options.failcount.getUrl(),
             default: '2',
         },
         keyboard: {
             get: () => document.getElementById('hideStenoKeyboard').checked? '0' : '1',
+            getDom: () => document.getElementById('hideStenoKeyboard').checked? '0' : '1',
             getUrl: () => urlParams.get('keyboard') || settings.options.keyboard.default,
             getBool: () => !document.getElementById('hideStenoKeyboard').checked,
             set: (keyboard) => {
@@ -62,72 +79,88 @@
                 if(hide) hideStenoKeyboard();
                 else showStenoKeyboard();
             },
+            domEqualUrl: () => settings.options.keyboard.getDom() === settings.options.keyboard.getUrl(),
             default: '1',
         },
         newCards: {
             get: () => document.getElementById('newCards').value,
+            getDom: () => document.getElementById('newCards').value,
             getUrl: () => urlParams.get('newCards') || settings.options.newCards.default,
             getInt: () => parseInt(document.getElementById('newCards').value),
             set: (newCards) => {
                 document.getElementById('newCards').value = newCards;
                 urlParams.set('newCards', newCards);
             },
+            domEqualUrl: () => settings.options.newCards.getDom() === settings.options.newCards.getUrl(),
             default: '10',
         },
         maxCards: {
             get: () => document.getElementById('maxCards').value,
+            getDom: () => document.getElementById('maxCards').value,
             getUrl: () => urlParams.get('maxCards') || settings.options.maxCards.default,
             getInt: () => parseInt(document.getElementById('maxCards').value),
             set: (maxCards) => {
                 document.getElementById('maxCards').value = maxCards;
                 urlParams.set('maxCards', maxCards);
             },
+            domEqualUrl: () => settings.options.maxCards.getDom() === settings.options.maxCards.getUrl(),
             default: '100',
         },
         quickSelect: {
             get: () => document.getElementById('quickSelect').checked? '1' : '0',
+            getDom: () => document.getElementById('quickSelect').checked? '1' : '0',
             getUrl: () => urlParams.get('quickSelect') || settings.options.quickSelect.default,
             set: (quickSelect) => {
                 document.getElementById('quickSelect').checked = (settings.options.quickSelect.default !== quickSelect);
                 urlParams.set('quickSelect', quickSelect);
             },
+            domEqualUrl: () => settings.options.quickSelect.getDom() === settings.options.quickSelect.getUrl(),
             default: '0',
         },
         dict: {
             get: () => document.querySelector('input[name="dictionary"]:checked').value,
+            getDom: () => document.querySelector('input[name="dictionary"]:checked').value,
             getUrl: () => urlParams.get('dict') || settings.options.dict.default,
             set: (dict) => {
                 radioSetter('dictionary', dict);
                 urlParams.set('dict', dict);
             },
+            domEqualUrl: () => settings.options.dict.getDom() === settings.options.dict.getUrl(),
             default: 'plover',
         },
         cardPrios: {
             get: () => document.querySelector('input[name="cardPrios"]:checked').value,
+            getDom: () => document.querySelector('input[name="cardPrios"]:checked').value,
             getUrl: () => urlParams.get('cardPrios') || settings.options.cardPrios.default,
             set: (cardPrios) => {
                 radioSetter('cardPrios', cardPrios);
                 urlParams.set('cardPrios', cardPrios);
             },
+            domEqualUrl: () => settings.options.cardPrios.getDom() === settings.options.cardPrios.getUrl(),
             default: 'newOverDue',
         },
         loop: {
             get: () => settings.options.loop.default,
+            getDom: () => settings.options.loop.default,
             getUrl: () => urlParams.get('loop') || settings.options.loop.default,
             set: (loop) => {},
+            domEqualUrl: () => settings.options.loop.getDom() === settings.options.loop.getUrl(),
             default: '1',
         },
         dbUrl: {
             get: () => document.getElementById('dbUrl').value,
+            getDom: () => document.getElementById('dbUrl').value,
             getUrl: () => urlParams.get('dbUrl') || settings.options.dbUrl.default,
             set: (dbUrl) => {
                 document.getElementById('dbUrl').value = dbUrl;
                 urlParams.set('dbUrl', dbUrl);
             },
+            domEqualUrl: () => settings.options.dbUrl.getDom() === settings.options.dbUrl.getUrl(),
             default: '',
         },
         exercise: {
             get: () => document.getElementById('isExercise').checked? '1' : '0',
+            getDom: () => document.getElementById('isExercise').checked? '1' : '0',
             getUrl: () => urlParams.get('isExercise') || settings.options.exercise.default,
             getBool: () => document.getElementById('isExercise').checked,
             set: (isExercise) => {
@@ -150,6 +183,7 @@
                     update_text_fct = textHandler;
                 }
             },
+            domEqualUrl: () => settings.options.exercise.getDom() === settings.options.exercise.getUrl(),
             default: '1',
         },
     };
@@ -192,27 +226,11 @@
         }, {});
     }
     setting.settingsChanged = () => {
-        const urlSettings = getUrlSettings();
-        const settings = getSettings();
-        const intersection = intersect(settings.tags, urlSettings.tags);
-        return !(intersection.length === settings.tags.length &&
-            settings.tags.length === urlSettings.tags.length &&
-            settings.randomize === urlSettings.randomize &&
-            settings.hand === urlSettings.hand &&
-            settings.scheduler === urlSettings.scheduler &&
-            settings.failcount === urlSettings.failcount &&
-            settings.keyboard === urlSettings.keyboard &&
-            settings.newCards === urlSettings.newCards &&
-            settings.maxCards === urlSettings.maxCards &&
-            settings.quickSelect === urlSettings.quickSelect &&
-            settings.dict === urlSettings.dict &&
-            settings.cardPrios === urlSettings.cardPrios &&
-            settings.dbUrl === urlSettings.dbUrl &&
-            settings.exercise === urlSettings.exercise &&
-            true
-        );
+        for(const key of Object.keys(settings)) {
+            if(!settings.options[key].domEqualUrl()) return true;
+        }
+        return false;
     }
-
     settings.saveSettings = (db) => {
         const textfield = getSettingsName();
         const name = textfield.value;

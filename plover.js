70efbc7d92b72c1cf0e59d4bfe18d92c6ff4fc89
Marco Kroll <marco.h.kroll@gmail.com>
2023-06-25 23:44:50 +0200

    Exercise: Modularize exercise code

--- plover.js
+++ plover.js
@@ -171,17 +171,6 @@ function shuffleArray(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
-function getExerciseData(ex, chap) {
-    for(let i = 0; i < exercises.length; ++i) {
-        if(exercises[i].name !== ex) continue;
-        for(let j = 0; j < exercises[i].chapters.length; ++j) {
-            const chapter = exercises[i].chapters[j];
-            if(chapter.name !== chap) continue;
-            return chapter.data;
-        }
-    }
-    return '';
-}
 function changeMax(event, val) {
     MAX_FAILURE = parseInt(val);
     showHint(0);
@@ -267,9 +256,6 @@ function getPreslection() {
     }
     return null;
 }
-function isFsrs() {
-    return settings.options.scheduler.get() === 'FSRS';
-}
 function isKeyboard() {
     return settings.options.keyboard.getBool();
 }
@@ -277,45 +263,10 @@ function showFsrsStats(tags) {
     const cardStats = document.getElementById('cardStats');
     cardStats.innerHTML = '';
     if(!isFsrs()) return ;
-    const filteredCards = filterCardsByTags(tags);
-    const minDue = cutOffDate(0);
-    const maxDue = cutOffDate();
-    const newMax = settings.options.newCards.getInt();
-    const cardsMax = settings.options.maxCards.getInt();
-    let cardsUndefined = 0;
-    let newCardsShownToday = 0;
-    let newCardsLearnedToday = 0;
-    let newCardsCount = 0;
-    let cardsShownToday = 0;
-    let cardsLearnedToday = 0;
-    let cardsDue = 0;
-    for(const c of filteredCards) {
-        if(c.scheduling === undefined) {
-            cardsUndefined += 1;
-            continue;
-        }
-        // all cards have scheduling information
-
-        // cards that have been reviewed today and that are new 
-        newCardsShownToday += ("New" !== c.state && c.scheduling.reviewLog[0].review >= minDue);
-        // cards that have been reviewed the first time today
-        newCardsCount += ("New" === c.state || c.scheduling.reviewLog[0].review >= minDue);
-        // new cards successfully learned today
-        newCardsLearnedToday += (c.scheduling.fsrsCard.due >= maxDue && c.scheduling.reviewLog[0].review >= minDue)
-
-        // cards reviewed
-        cardsShownToday += (c.scheduling.reviewLog.at(-1).review >= minDue);
-        // cards not reviewed, but due
-        cardsDue += (c.scheduling.fsrsCard.due < maxDue && !(c.scheduling.reviewLog.at(-1).review >= minDue));
-        // cards successfully learned today
-        cardsLearnedToday += (c.scheduling.fsrsCard.due >= maxDue && c.scheduling.reviewLog.at(-1).review >= minDue);
-    }
-    const newCardsMax = Math.min(cardsUndefined + newCardsCount, newMax);
-    const dueCardsMax = Math.min(cardsUndefined + cardsShownToday + cardsDue, cardsMax);
-    // console.log(`cardsUndefined: ${cardsUndefined}, newCardsShownToday ${newCardsShownToday}, newCardsCount: ${newCardsCount}, newCardsLearnedToday ${newCardsLearnedToday}, newMax: ${newMax}, newCardsMax: ${newCardsMax}`);
-    // console.log(`cardsUndefined: ${cardsUndefined}, cardsLearnedToday: ${cardsLearnedToday}, cardsShownToday: ${cardsShownToday}, cardsDue: ${cardsDue}, cardsMax: ${cardsMax}, dueCardsMax: ${dueCardsMax}`);
-
-    cardStats.innerHTML = 'New: ' + newCardsLearnedToday + '/' + newCardsShownToday + '/' + newCardsMax + ' Total: ' + cardsLearnedToday + '/' + cardsShownToday + '/' + dueCardsMax + '/' + filteredCards.length;
+    const stats = getFsrsStats(tags);
+    cardStats.innerHTML =
+        'New: ' + stats.newCardsLearnedToday + '/' + stats.newCardsShownToday + '/' + stats.newCardsMax + ' ' +
+        'Total: ' + stats.cardsLearnedToday + '/' + stats.cardsShownToday + '/' + stats.dueCardsMax + '/' + stats.total;
 }
 function textHandler(t, s) {
     exercise.innerHTML = t;
@@ -506,160 +457,6 @@ function intersect(a, b) {
   var setB = new Set(b);
   return [...new Set(a)].filter(x => setB.has(x));
 }
-function annotateCardsWithLocalData(cards) { // modifies input
-    const promises = [];
-    for(const card of cards) {
-        const id = card.collection + '::' + card.word;
-        promises.push(
-            db.get(id).then(data => {
-                if(null !== data) card.scheduling = data;
-                return card;
-            }).catch(err => {
-                if('missing' === err.reason) return card;
-                throw err;
-            }));
-    }
-    return Promise.all(promises);
-}
-function orderCardsDueAndNew(cards) {
-    const maxDue = cutOffDate();
-    const minDue = cutOffDate(0);
-    const nonCards = [];
-    const newCards = [];
-    const lrnCards = [];
-    const revCards = [];
-    const rlnCards = [];
-    const dueCards = [];
-    const newCardsLearnedToday = cards.filter(c => (c.scheduling !== undefined &&
-        c.scheduling.reviewLog.at(0).review >= minDue &&
-        c.scheduling.fsrsCard.due >= maxDue)).length;
-    const cardsLearnedToday = cards.filter(c => (c.scheduling !== undefined &&
-        c.scheduling.reviewLog.at(-1).review >= minDue &&
-        c.scheduling.fsrsCard.due >= maxDue)).length;
-    const newCardsMax = Math.max(0, settings.options.newCards.getInt() - newCardsLearnedToday);
-    const cardsMax = Math.max(0, settings.options.maxCards.getInt() - cardsLearnedToday);
-    for(let i = 0; i < cards.length; ++i) {
-        // only include cards that are new or due this day
-        if(undefined !== cards[i].scheduling &&
-            cards[i].scheduling.fsrsCard.due >= maxDue) {
-            continue;
-        }
-        if(undefined === cards[i].scheduling) {
-            nonCards.push(cards[i]);
-            continue;
-        }
-        const state = cards[i].scheduling.fsrsCard.state;
-        if(cards[i].scheduling.reviewLog.at(-1).review >= minDue) {
-            dueCards.push(cards[i]);
-        } else if("New" == state) {
-            newCards.push(cards[i]);
-        } else if("Learning" == state) {
-            lrnCards.push(cards[i]);
-        } else if("Relearning" == state) {
-            rlnCards.push(cards[i]);
-        } else if("Review" == state) {
-            revCards.push(cards[i]);
-        } else {
-            console.log('Invalid state found: ' + cards[i]);
-        }
-    }
-    // sort by due date (before rank was used for stability)
-    const cmp = (a, b) => a.scheduling.fsrsCard.due.localeCompare(b.scheduling.fsrsCard.due);
-    dueCards.sort(cmp);
-    newCards.sort(cmp);
-    lrnCards.sort(cmp);
-    rlnCards.sort(cmp);
-    revCards.sort(cmp);
-    // non cards are sorted by rank or if that is not possible by alphabetical order
-    nonCards.sort((a, b) => {
-        if(undefined === a.rank && undefined !== b.rank) return 1
-        if(undefined !== a.rank && undefined === b.rank) return -1
-        if(undefined === a.rank && undefined === b.rank) return a.word.localeCompare(b.word);
-        return a.rank - b.rank;
-    });
-    // new and unseen cards
-    newCards.push(...nonCards);
-    newCards.length = Math.min(newCards.length, newCardsMax);
-    // learn, relearn and review cards
-    dueCards.push(...lrnCards); // first learn to increase knowledge
-    dueCards.push(...rlnCards); // next relearn to get back lost knowledge
-    dueCards.push(...revCards); // finally review to steady knowledge
-    dueCards.length = Math.min(dueCards.length, cardsMax);
-    if(settings.options.cardPrios.get() === 'dueOverNew') {
-        result = dueCards.concat(newCards);
-    } else {
-        result = newCards.concat(dueCards);
-    }
-    result.length = Math.min(result.length, cardsMax);
-    if(settings.options.randomize.getBool()) {
-        shuffleArray(result);
-    }
-    return result;
-}
-function getDifficultCards() {
-    const maxCards = settings.options.maxCards.getInt();
-    return db.allDocs({include_docs: true}).then(response => {
-        difficultCards = response.rows
-            .filter(a => a.doc._id.startsWith('all::'))
-            .sort((a, b) => b.doc.fsrsCard.difficulty - a.doc.fsrsCard.difficulty);
-        difficultCards.length = maxCards;
-
-        result = difficultCards.map(c => {
-            const s = c.id.split('::').filter(s => '' !== s);
-            if(s.length == 2) {
-                const collection = cards[s[0]];
-                const name = s[1];
-                if(collection === undefined) return undefined;
-                collection.cards[name].scheduling = c.doc;
-                return collection.cards[name]
-            }
-            return undefined
-        }).filter(a => a !== undefined);
-
-        if(settings.options.randomize.getBool()) {
-            shuffleArray(result);
-        }
-        return result;
-    });
-}
-// Fully Qualified Tags to tags per collection
-function fqtags2tagsByCollection(tags) {
-    const tagsByCollection = tags.reduce(function(acc, cur, i) {
-        const s = cur.split('::').filter(s => '' !== s);
-        if(s.length == 2) {
-            const collection = s[0];
-            const tag = s[1];
-            if(undefined === acc[collection]) {
-                acc[collection] = [];
-            }
-            acc[collection].push(tag);
-        }
-        return acc;
-    }, {});
-    return tagsByCollection;
-}
-function filterCardsByTags(fqtags) {
-    const tagsByCollection = fqtags2tagsByCollection(fqtags);
-    const result = [];
-    for(const name of Object.keys(tagsByCollection)) {
-        const collection = cards[name];
-        if(collection === undefined) continue;
-        const tags = tagsByCollection[name];
-        const filteredCards = Object.values(collection.cards).filter(c => intersect(c.tags, tags).length > 0);
-        result.push(...filteredCards);
-        result.map(c => c.collection = name);
-    }
-    return result;
-}
-function getCards(tags) {
-    const filteredCards = filterCardsByTags(tags);
-    if(isFsrs()) {
-        return annotateCardsWithLocalData(filteredCards).then(cards => orderCardsDueAndNew(cards));
-    } else if(settings.options.randomize.getBool()) {
-        shuffleArray(filteredCards);
-    }
-    return Promise.resolve(filteredCards);
-}
 function py2js(pyObj) {
     const toObject = (map = new Map) =>
         Object.fromEntries
@@ -673,55 +470,6 @@ function py2js(pyObj) {
     );
     return toObject(pyObj.toJs());
 }
-function getEstimate(due) {
-    const now = new Date;
-    const next = new Date(due);
-    const diffM = Math.ceil((next - now) / 60000);
-    if(diffM <= 5) return `&lt; ${diffM} minute${diffM === 1? '' : 's'}`;
-    if(diffM <= 55) return `&lt; ${Math.ceil(diffM / 5) * 5} Minutes`;
-    const diffH = Math.ceil(diffM / 60);
-    if(diffM < 24) return `&lt; ${diffH} hour${diffH === 1? '' : 's'}`;
-    const diffD = Math.ceil(diffH / 24);
-    if(diffD < 7) return `&lt; ${diffD} day${diffD === 1? '' : 's'}`;
-    const diffW = Math.ceil(diffD / 7);
-    if(diffW < 5) return `&lt; ${diffW} week${diffW === 1? '' : 's'}`;
-    const diffMon = Math.ceil(diffD / 30.416);
-    if(diffW < 13) return `&lt; ${diffMon} month${diffMon === 1? '' : 's'}`;
-    const diffY = Math.ceil(diffD / 365);
-    return `&lt; ${diffY} year${diffY === 1? '' : 's'}`;
-}
-function getCardDifficulties(card) {
-    const now = new Date().toISOString();
-    const fsrsPy = pyscript.interpreter.globals.get('fsrs');
-    if(undefined === card.scheduling) {
-        const cardPy = fsrsPy.newCardJs();
-        const data = py2js(cardPy);
-        card.scheduling = {
-            fsrsCard: data,
-            reviewLog: []
-        };
-    }
-    const newCardsPy = fsrsPy.repeatJs(card.scheduling.fsrsCard, now);
-    return py2js(newCardsPy);
-}
-function putCardBack2(card, answer, ease) {
-    const newCards = getCardDifficulties(card);
-    newCards[ease].review_log.answer = answer;
-    card.scheduling.fsrsCard = newCards[ease].card;
-    card.scheduling.reviewLog.push(newCards[ease].review_log);
-    card.scheduling._id = card.collection + '::' + card.word;
-    db.put(card.scheduling, function(err, response) {
-        if (err) { return console.log(err); }
-        // handle response
-        console.log(response);
-    });
-    console.log(card.scheduling);
-    hideDifficulty();
-
-    ++currentExerciseIndex;
-    exercise.innerHTML = textToLength().join('\n');
-    initNextExercise();
-}
 let putCardBack = ease => putCardBack2(null, null, ease);
 function onAgain(event) {
     putCardBack('Again');
@@ -734,18 +482,6 @@ function onGood(event) {
 }
 function onEasy(event) {
     putCardBack('Easy');
-}
-function getTagsFromUrl(urlParams) {
-    return (urlParams.get('tags') || '').split(',').filter(s => '' !== s);
-}
-function getTagsFromSettings() {
-    return Object.values(document.getElementsByClassName("tagCheckbox")).filter(c => c.checked).map(c => c.id);
-}
-function setTagsInSettings(tags) {
-    const tagSet = new Set(tags);
-    Object.values(document.getElementsByClassName("tagCheckbox")).map(c => {
-        c.setAttribute('checked', tagSet.has(c.id));
-    });
 }
 function loadCards(deck) {
     fetch(deck)
